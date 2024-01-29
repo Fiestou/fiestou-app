94af96aedffe55843c852636c15b04ce15b7c1a7
rafael8p <rafaelcarvalhosjrp@gmail.com>
2024-01-29 16:19:30 -0300

    feat: sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,15 @@ module.exports = {
   siteUrl: process.env.APP_URL || "https://www.fiestou.com.br",
   changefreq: "daily",
   generateRobotsTxt: true,
+  exclude: [
+    "/admin/*",
+    "/dashboard/*",
+    "/painel/*",
+    "/api/*",
+    "/checkout",
+    "/finalizar",
+    "/carrinho",
+    "/login",
+    "/logout",
+  ],
 };
