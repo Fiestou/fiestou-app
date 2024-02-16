c59dbca89073177df6c819e01cf64e784155cadf
rafael8p <rafaelcarvalhosjrp@gmail.com>
2024-02-16 15:56:09 -0300

    fix: upload de imagem / Envio de SMS

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -25,27 +25,45 @@ class SmsController extends Controller
             'content' => 'required',
         ]);
 
-        $to         = trim(preg_replace("/[^0-9]/", "", $request->input('phone')));
-        $message    = $request->input('content');
 
-        $twilio_sid             = env('TWILIO_SID');
-        $twilio_token           = env('TWILIO_AUTH_TOKEN');
-        $twilio_phone_number    = env('TWILIO_PHONE_NUMBER');
+        $to = trim(preg_replace("/[^0-9]/", "", $request->input('phone')));
+        $to = (substr($to, 0, 2) !== "55") ? "+55".$to : "+".$to;
 
-        $twilio = new Client($twilio_sid, $twilio_token);
+        $message = strip_tags(nl2br(str_replace("<br>", "\n", $request->input('content'))));
 
-        $twilio->messages->create(
-            // "+55".$to,
-            "+5583999791586",
-            [
-                "body" => nl2br(strip_tags($message)),
-                "from" => $twilio_phone_number
-            ]
-        );
+        try {
 
-        return response()->json([
-            'response'  => true,
-            'message'   => "sended",
-        ], 200);
+            $twilio_sid             = env('TWILIO_SID');
+            $twilio_token           = env('TWILIO_AUTH_TOKEN');
+            $twilio_phone_number    = env('TWILIO_PHONE_NUMBER');
+
+            $twilio = new Client($twilio_sid, $twilio_token);
+
+            $request = $twilio->messages->create(
+                $to,
+                [
+                    "body" => "Fiestou: " . $message,
+                    "from" => $twilio_phone_number
+                ]
+            );
+
+            return response()->json([
+                'response'  => true,
+                'message'   => [
+                        "to"    => $to,
+                        "body"  => $message,
+                        "from"  => $twilio_phone_number,
+                        "log"   => $request->sid
+                    ],
+            ], 200);
+        }
+        catch (\Exception $e) {
+
+            return response()->json([
+                'response'  => false,
+                'phone'     => $to,
+                'message'   => $e->getMessage(),
+            ], 500);
+        }
     }
 }
