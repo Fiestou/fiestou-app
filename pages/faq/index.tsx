00d5299b3871fcb3d941b5ee36af622fb80811b7
Gregório Alencar <rgasti@outlook.com>
2025-08-29 17:29:34 -0300

    alterações sexta-feira

--- pages/faq/index.tsx
+++ pages/faq/index.tsx
@@ -1,20 +1,16 @@
 import Api from "@/src/services/api";
 import Template from "@/src/template";
-import Link from "next/link";
-import { FormEvent, useState } from "react";
-import { useRouter } from "next/router";
-import ButtonLoader from "@/src/components/utils/ButtonLoader";
+import { useState } from "react";
 import Img from "@/src/components/utils/ImgBase";
-import { Button, Input, Label } from "@/src/components/ui/form";
+import { Button } from "@/src/components/ui/form";
 import Icon from "@/src/icons/fontAwesome/FIcon";
-import Newsletter from "@/src/components/common/Newsletter";
-import { cleanText, getImage } from "@/src/helper";
+import { getImage } from "@/src/helper";
 import Breadcrumbs from "@/src/components/common/Breadcrumb";
 
 export async function getStaticProps(ctx: any) {
   const api = new Api();
 
-  let request: any = await api.content({method: 'get', url: `faq` });
+  let request: any = await api.content({ method: 'get', url: `faq` });
 
   const Faq = request?.data?.Faq ?? {};
   const HeaderFooter = request?.data?.HeaderFooter ?? {};
@@ -75,21 +71,11 @@ export default function Ajuda({
               </div>
               <h1
                 className="font-title font-bold text-4xl md:text-5xl mb-4"
-                dangerouslySetInnerHTML={{ __html: Faq?.main_text }}
-              ></h1>
+              >Dúvidas</h1>
               <div
                 className="text-lg md:text-2xl font-semibold"
-                dangerouslySetInnerHTML={{ __html: Faq?.main_description }}
-              ></div>
+              >Temos as respostas</div>
             </div>
-            {!!getImage(Faq?.main_icons) && (
-              <div className="w-fit">
-                <Img
-                  src={getImage(Faq?.main_icons)}
-                  className="w-auto max-w-full"
-                />
-              </div>
-            )}
           </div>
         </div>
       </section>
@@ -97,62 +83,177 @@ export default function Ajuda({
       <section className="py-1 md:py-10">
         <div className="container-medium">
           <div className="">
-            {!!Faq?.main_text &&
-              Faq?.faq_list.map((item: any, key: any) => (
-                <div key={key} className="border-b py-6">
-                  <div
-                    onClick={() =>
-                      setCollapseFaq(key != collapseFaq ? key : -1)
-                    }
-                    className="font-title flex font-bold text-zinc-900 md:text-2xl cursor-pointer"
-                  >
-                    <span className="w-full">{item.answer_question}</span>
-                    <div>
-                      <Icon
-                        icon={
-                          collapseFaq == key
-                            ? "fa-chevron-up"
-                            : "fa-chevron-down"
-                        }
-                        type="far"
-                        className="text-sm"
-                      />
-                    </div>
-                  </div>
-                  {collapseFaq == key && (
-                    <div
-                      className="pt-4"
-                      dangerouslySetInnerHTML={{ __html: item.answer_text }}
-                    ></div>
-                  )}
+            <div className="border-b py-6">
+              <div
+                onClick={() => setCollapseFaq(collapseFaq !== 0 ? 0 : -1)}
+                className="font-title flex font-bold text-zinc-900 md:text-2xl cursor-pointer"
+              >
+                <span className="w-full">Como funciona?</span>
+                <div>
+                  <Icon
+                    icon={collapseFaq === 0 ? "fa-chevron-up" : "fa-chevron-down"}
+                    type="far"
+                    className="text-sm"
+                  />
                 </div>
-              ))}
+              </div>
+              {collapseFaq === 0 && (
+                <div className="pt-4">
+                  Procure o que precise para sua festa, reserve, faça o pagamento. E entregamos no dia marcado.
+                </div>
+              )}
+            </div>
+
+            <div className="border-b py-6">
+              <div
+                onClick={() => setCollapseFaq(collapseFaq !== 1 ? 1 : -1)}
+                className="font-title flex font-bold text-zinc-900 md:text-2xl cursor-pointer"
+              >
+                <span className="w-full">Que produtos a Fiestou oferece?</span>
+                <div>
+                  <Icon
+                    icon={collapseFaq === 1 ? "fa-chevron-up" : "fa-chevron-down"}
+                    type="far"
+                    className="text-sm"
+                  />
+                </div>
+              </div>
+              {collapseFaq === 1 && (
+                <div className="pt-4">
+                  Oferecemos decorações, buffet e bolo para a festa.
+                </div>
+              )}
+            </div>
+            <div className="border-b py-6">
+              <div
+                onClick={() => setCollapseFaq(collapseFaq !== 2 ? 2 : -1)}
+                className="font-title flex font-bold text-zinc-900 md:text-2xl cursor-pointer"
+              >
+                <span className="w-full">Onde pode ser feito a entrega?</span>
+                <div>
+                  <Icon
+                    icon={collapseFaq === 2 ? "fa-chevron-up" : "fa-chevron-down"}
+                    type="far"
+                    className="text-sm"
+                  />
+                </div>
+              </div>
+              {collapseFaq === 2 && (
+                <div className="pt-4">
+                  Na cidade de João Pessoa.
+                </div>
+              )}
+            </div>
+            <div className="border-b py-6">
+              <div
+                onClick={() => setCollapseFaq(collapseFaq !== 3 ? 3 : -1)}
+                className="font-title flex font-bold text-zinc-900 md:text-2xl cursor-pointer"
+              >
+                <span className="w-full">Onde nos localizamos?</span>
+                <div>
+                  <Icon
+                    icon={collapseFaq === 3 ? "fa-chevron-up" : "fa-chevron-down"}
+                    type="far"
+                    className="text-sm"
+                  />
+                </div>
+              </div>
+              {collapseFaq === 3 && (
+                <div className="pt-4">
+                  Temos um depósito na Rua Alfonso Ramos Maia 77 - Miramar. Mas a gente faz a entrega no lugar que você deseja. Menos trabalho para você!
+                </div>
+              )}
+            </div>
+            <div className="border-b py-6">
+              <div
+                onClick={() => setCollapseFaq(collapseFaq !== 4 ? 4 : -1)}
+                className="font-title flex font-bold text-zinc-900 md:text-2xl cursor-pointer"
+              >
+                <span className="w-full">Quanto custa?</span>
+                <div>
+                  <Icon
+                    icon={collapseFaq === 4 ? "fa-chevron-up" : "fa-chevron-down"}
+                    type="far"
+                    className="text-sm"
+                  />
+                </div>
+              </div>
+              {collapseFaq === 4 && (
+                <div className="pt-4">
+                  Temos diversos produtos, com os valores nos cards.
+                </div>
+              )}
+            </div>
+            <div className="border-b py-6">
+              <div
+                onClick={() => setCollapseFaq(collapseFaq !== 5 ? 5 : -1)}
+                className="font-title flex font-bold text-zinc-900 md:text-2xl cursor-pointer"
+              >
+                <span className="w-full">A Fiestou faz a montagem da decoração?</span>
+                <div>
+                  <Icon
+                    icon={collapseFaq === 5 ? "fa-chevron-up" : "fa-chevron-down"}
+                    type="far"
+                    className="text-sm"
+                  />
+                </div>
+              </div>
+              {collapseFaq === 5 && (
+                <div className="pt-4">
+                  Na descrição dos produtos, diz se está incluído a montagem ou não.
+                </div>
+              )}
+            </div>
+            <div className="border-b py-6">
+              <div
+                onClick={() => setCollapseFaq(collapseFaq !== 6 ? 6 : -1)}
+                className="font-title flex font-bold text-zinc-900 md:text-2xl cursor-pointer"
+              >
+                <span className="w-full">Onde posso baixar o aplicativo?</span>
+                <div>
+                  <Icon
+                    icon={collapseFaq === 6 ? "fa-chevron-up" : "fa-chevron-down"}
+                    type="far"
+                    className="text-sm"
+                  />
+                </div>
+              </div>
+              {collapseFaq === 6 && (
+                <div className="pt-4">
+                  No momento não temos aplicativo. Somos um website que pode acessar em qualquer navegador.
+                </div>
+              )}
+            </div>
           </div>
+
         </div>
       </section>
 
       <section>
         <div className="container-medium py-14">
-  <div className="bg-zinc-100 border border-zinc-100 rounded-xl grid lg:flex items-center relative overflow-hidden">
-    <div className="w-full grid gap-6 p-6 md:p-16 text-zinc-900">
-      <h4 className="font-title font-bold max-w-[30rem] text-3xl md:text-4xl">
-        Fale com a nossa equipe e tire todas as suas dúvidas!
-      </h4>
+          <div className="bg-zinc-100 border border-zinc-100 rounded-xl grid lg:flex items-center relative overflow-hidden">
+            <div className="w-full grid gap-6 p-6 md:p-16 text-zinc-900">
+              <h4 className="font-title font-bold max-w-[30rem] text-3xl md:text-4xl">
+                Fale com a nossa equipe e tire todas as suas dúvidas!
+              </h4>
 
-      <div className="pt-2">
-        <Button href="/contato">Enviar mensagem</Button>
-      </div>
-    </div>
+              <div className="pt-2">
+                <Button
+                  href="https://wa.me/5583999812030?text=Ol%C3%A1%2C%20gostaria%20de%20tirar%20uma%20d%C3%BAvida."
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Enviar mensagem
+                </Button>
+              </div>
+            </div>
 
-    <div className="w-full">
-      <Img
-        className="w-full"
-        src="/images/faq-banner.png" // <-- aqui você coloca a imagem fixa
-        alt="Entre em contato conosco"
-      />
-    </div>
-  </div>
-</div>
+            <div className="w-full">
+              <Img className="w-full" src={getImage(Faq?.cta_image)} />
+            </div>
+          </div>
+        </div>
+
 
       </section>
     </Template>
--- pages/sobre/index.tsx
+++ pages/sobre/index.tsx
@@ -6,21 +6,28 @@ import { useRouter } from "next/router";
 import ButtonLoader from "@/src/components/utils/ButtonLoader";
 import Img from "@/src/components/utils/ImgBase";
 import { Button, Input, Label } from "@/src/components/ui/form";
-import Icon from "@/src/icons/fontAwesome/FIcon";
 import Newsletter from "@/src/components/common/Newsletter";
 import { clean, getImage } from "@/src/helper";
-
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
 import Breadcrumbs from "@/src/components/common/Breadcrumb";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHandPointUp, faCalendar, faWineBottle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+// Configura a biblioteca do Font Awesome
+library.add(faHandPointUp, faCalendar, faWineBottle);
+
+// Tipagem para as props (se usar TypeScript)
+interface SobreProps {
+  About: any;
+  HeaderFooter: any;
+  DataSeo: any;
+  Scripts: any;
+}
+
+// Função getStaticProps
 export async function getStaticProps(ctx: any) {
   const api = new Api();
-
-  let request: any = await api.content({method: 'get', url: `about` });
+  let request: any = await api.content({ method: 'get', url: `about` });
 
   const About = request?.data?.About ?? {};
   const HeaderFooter = request?.data?.HeaderFooter ?? {};
@@ -29,34 +36,41 @@ export async function getStaticProps(ctx: any) {
 
   return {
     props: {
-      About: About,
-      HeaderFooter: HeaderFooter,
-      DataSeo: DataSeo,
-      Scripts: Scripts,
+      About,
+      HeaderFooter,
+      DataSeo,
+      Scripts,
     },
     revalidate: 60 * 60 * 60,
   };
 }
 
-export default function Sobre({
-  About,
-  HeaderFooter,
-  DataSeo,
-  Scripts,
-}: {
-  About: any;
-  HeaderFooter: any;
-  DataSeo: any;
-  Scripts: any;
-}) {
+// Dados dos itens
+const worksList = [
+  {
+    work_title: 'Clicou',
+    work_description: 'Procure no site pelas melhores ofertas de fornecedores no setor de eventos, pesquisando por produtos ou por parceiros. Em alguns clicks sua festa está pronta.',
+    work_icon: faHandPointUp, // Alterado de faCircle para fa-hand-point-up
+  },
+  {
+    work_title: 'Marcou',
+    work_description: 'Na data e horário marcado, a entrega será feita no endereço registrado. Pontualmente e com o acompanhamento pelo portal do usuário.',
+    work_icon: faCalendar,
+  },
+  {
+    work_title: 'Fiestou',
+    work_description: 'Aproveite sua festa. No dia seguinte recolhemos, o que for alugado. E dá uma forcinha para gente e avalie o serviço.',
+    work_icon: faWineBottle,
+  },
+];
+
+export default function Sobre({ About, HeaderFooter, DataSeo, Scripts }: SobreProps) {
   return (
     <Template
       scripts={Scripts}
       metaPage={{
         title: `Sobre | ${DataSeo?.site_text}`,
-        image: !!getImage(DataSeo?.site_image)
-          ? getImage(DataSeo?.site_image)
-          : "",
+        image: getImage(DataSeo?.site_image) || "",
         description: clean(About?.main_description),
         url: `sobre`,
       }}
@@ -70,84 +84,57 @@ export default function Sobre({
         content: HeaderFooter,
       }}
     >
-      <section className="bg-cyan-500  pt-24 md:pt-32 relative">
+      <section className="bg-cyan-500 pt-24 md:pt-32 relative">
         <div className="container-medium relative pb-4 md:pb-10 text-white">
           <div className="flex">
             <div className="w-full">
               <div className="pb-4">
                 <Breadcrumbs links={[{ url: "/sobre", name: "Sobre" }]} />
               </div>
-              <h1
-                className="font-title font-bold text-4xl md:text-5xl mb-4"
-                dangerouslySetInnerHTML={{ __html: About?.main_text }}
-              ></h1>
-              <div
-                className="text-lg md:text-2xl font-semibold"
-                dangerouslySetInnerHTML={{ __html: About?.main_description }}
-              ></div>
-            </div>
-            {!!getImage(About?.main_icons) && (
-              <div className="w-fit">
-                <Img
-                  src={getImage(About?.main_icons)}
-                  className="w-auto max-w-full"
-                />
+              <h1 className="font-title font-bold text-4xl md:text-5xl mb-4">
+                Sobre nós
+              </h1>
+              <div className="text-lg md:text-2xl font-semibold">
+                Uma start-up para facilitar em como realizar sua festa
               </div>
-            )}
+            </div>
           </div>
         </div>
       </section>
 
-      <section className="py-10 md:py-20 relative overflow-hidden">
+      <section className="py-10 md:pt-20 relative overflow-hidden">
         <div className="container-medium">
           <div className="max-w-3xl mx-auto text-center pb-6 md:pb-14">
-            <h2
-              className="font-title text-zinc-900 font-bold text-3xl md:text-5xl mt-2"
-              dangerouslySetInnerHTML={{ __html: About?.works_text }}
-            ></h2>
+            <h2 className="font-title text-zinc-900 font-bold text-3xl md:text-5xl mt-2">
+              Te ajudamos a festejar sem dor de cabeça
+            </h2>
           </div>
-          <div className="">
-            <Swiper
-              spaceBetween={16}
-              modules={[Navigation]}
-              breakpoints={{
-                0: {
-                  slidesPerView: 1.25,
-                },
-                640: {
-                  slidesPerView: 2.5,
-                },
-                1024: {
-                  slidesPerView: 3,
-                },
-              }}
-              className="swiper-equal swiper-visible"
-            >
-              {!!About?.works_text &&
-                About?.works_list.map((item: any, key: any) => (
-                  <SwiperSlide key={key}>
-                    <div className="border h-full rounded-lg p-6 md:p-10">
-                      <div className="p-8 text-yellow-400 relative">
-                        <Icon
-                          icon={item.work_image ?? "fa-hand-point-up"}
-                          className="text-6xl absolute top-1/2 left-0 -translate-y-1/2"
-                        />
-                        <Icon
-                          icon={item.work_image ?? "fa-hand-point-up"}
-                          type="fa"
-                          className="text-5xl mt-1 opacity-20 absolute top-1/2 left-0 -translate-y-1/2"
-                        />
-                      </div>
-                      <div className="pt-6">
-                        <h3 className="font-title text-zinc-900 text-2xl font-bold pb-4">
-                          {item.work_title}
-                        </h3>
-                        <div>{item.work_description}</div>
-                      </div>
-                    </div>
-                  </SwiperSlide>
-                ))}
-            </Swiper>
+          <div className="px-4 py-8">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+              {worksList.map((item, index) => (
+                <div
+                  key={index}
+                  className="border h-full rounded-lg p-6 md:p-10 bg-white"
+                >
+                  <div className="p-8 text-yellow-400 relative">
+                    <FontAwesomeIcon
+                      icon={item.work_icon}
+                      className="text-6xl absolute top-1/2 left-0 -translate-y-1/2"
+                    />
+                    <FontAwesomeIcon
+                      icon={item.work_icon}
+                      className="text-5xl mt-1 opacity-20 absolute top-1/2 left-0 -translate-y-1/2"
+                    />
+                  </div>
+                  <div className="pt-6">
+                    <h3 className="font-title text-zinc-900 text-2xl font-bold pb-4">
+                      {item.work_title}
+                    </h3>
+                    <div className="text-gray-600">{item.work_description}</div>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </section>
@@ -175,61 +162,6 @@ export default function Sobre({
                 </div>
               )}
             </div>
-          </div>
-        </div>
-      </section>
-
-      <section className="py-10 md:pt-20 relative overflow-hidden">
-        <div className="container-medium">
-          <div className="max-w-3xl mx-auto text-center pb-6 md:pb-14">
-            <span>{About?.args_title}</span>
-            <h2
-              className="font-title text-zinc-900 font-bold text-3xl md:text-5xl mt-2"
-              dangerouslySetInnerHTML={{ __html: About?.args_text }}
-            ></h2>
-          </div>
-          <div className="">
-            <Swiper
-              spaceBetween={16}
-              modules={[Navigation]}
-              breakpoints={{
-                0: {
-                  slidesPerView: 1.25,
-                },
-                640: {
-                  slidesPerView: 2.5,
-                },
-                1024: {
-                  slidesPerView: 3,
-                },
-              }}
-              className="swiper-equal swiper-visible"
-            >
-              {!!About?.args_list &&
-                About?.args_list.map((item: any, key: any) => (
-                  <SwiperSlide key={key}>
-                    <div className="border h-full rounded-lg p-6 md:p-10">
-                      <div className="p-8 text-yellow-400 relative">
-                        <Icon
-                          icon={item.arg_image ?? "fa-hand-point-up"}
-                          className="text-6xl absolute top-1/2 left-0 -translate-y-1/2"
-                        />
-                        <Icon
-                          icon={item.arg_image ?? "fa-hand-point-up"}
-                          type="fa"
-                          className="text-5xl mt-1 opacity-20 absolute top-1/2 left-0 -translate-y-1/2"
-                        />
-                      </div>
-                      <div className="pt-6">
-                        <h3 className="font-title text-zinc-900 text-2xl font-bold pb-4">
-                          {item.arg_title}
-                        </h3>
-                        <div>{item.arg_description}</div>
-                      </div>
-                    </div>
-                  </SwiperSlide>
-                ))}
-            </Swiper>
           </div>
         </div>
       </section>
