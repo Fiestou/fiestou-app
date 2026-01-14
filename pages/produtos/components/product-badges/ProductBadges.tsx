72da189099f82c330ab68ee83fe10d6aebbe5903
Gregório Alencar <rgasti@outlook.com>
2026-01-14 08:27:29 -0300

    feat: update commercial type display in Product and ProductBadges components for better clarity

--- pages/produtos/components/product-badges/ProductBadges.tsx
+++ pages/produtos/components/product-badges/ProductBadges.tsx
@@ -39,12 +39,28 @@ export default function ProductBadges({
       </div>
 
       {/* Tipo comercial */}
-      <div className="flex gap-1 ">
+      <div className="flex gap-1">
         <span>Disponível para:</span>
-        <Badge style="light" className="bg-blue-200 px-1">
-          {(product.comercialType as string).charAt(0).toUpperCase() +
-            (product.comercialType as string).slice(1)}
-        </Badge>
+
+        {(product.comercialType as string) === "venda" && (
+          <Badge
+            style="light"
+            className="bg-red-200 px-1 flex gap-1 items-center"
+          >
+            <Icon icon="fa-tag" className="text-xs" type="far" />
+            <span>Venda</span>
+          </Badge>
+        )}
+
+        {(product.comercialType as string) === "aluguel" && (
+          <Badge
+            style="light"
+            className="bg-blue-200 px-1 flex gap-1 items-center"
+          >
+            <Icon icon="fa-clock" className="text-xs" type="far" />
+            <span>Aluguel</span>
+          </Badge>
+        )}
       </div>
     </div>
   );
