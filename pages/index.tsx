dc380d5cdc7ac1bd622e14ac0fd3a68b282a982f
pedro <pedroevil51@gmail.com>
2026-05-01 19:56:39 +0000

    home: corrige encoding, remove slice redundante e fix key prop

--- pages/index.tsx
+++ pages/index.tsx
@@ -210,7 +210,7 @@ export default function Home({
           <Filter />
         </div>
       </div>
-      <ProductSection products={Products.slice(0, 5)} />
+      <ProductSection products={Products} />
 
       <LazyRender
         minHeight={340}
